2444bdb53d807c7467063d2202291539a5d219df
Dayane Ramos <dayanecode@gmail.com>
2023-10-30 18:18:18 -0300

    create app.js

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ require ("dotenv").config()
 
 const express = require("express");
 const path = require("path");
+
 const cors = require("cors");
 
 const port = process.env.PORT;
 
 const app = express()
 
-//config JSON and form data response
+//config JSON and f orm data response
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Solve CORS
+
 app.use(cors({ credentials: true, origin: "http://localhost:3000"}))
 
 // Upload directory
