7c3c3158e0ebc3d987003c0cd4b870cdc41119fa
dayanecode <dayanecode@gmail.com>
2023-10-30 21:46:37 -0300

    add uploads folder and insert image

--- middlewares/imageUpload.js
+++ middlewares/imageUpload.js
@@ -1,28 +1,47 @@
 const multer = require("multer")
 const path = require("path")
+const fs = require('fs')
+
 
 // Destination to store image
 const imageStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        let folder = ""
 
-        if (req.baseUrl.includes("users")) {
-            folder = "users"
-        } else if (req.baseUrl.includes("photos")) {
-            folder = "photos"
+        let uploadsFolder = path.join(__dirname, "../uploads");
+
+        // garante que a pasta 'uploads' exista!
+        if (!fs.existsSync(uploadsFolder)) {
+            fs.mkdirSync(uploadsFolder)
         }
 
-        cb(null, `uploads/${folder}/`)
+        let folder = ""
+
+        // Verifica se a base URL inclui users and photos
+        if (req.baseUrl.includes("users")) {
+            let folder = path.join(__dirname, '../uploads/users');
+            if (!fs.existsSync(folder)) {
+                fs.mkdirSync(folder);
+            }
+        } else if (req.baseUrl.includes("photos")) {
+            let folder = path.join(__dirname, '../uploads/photos');
+            if (!fs.existsSync(folder)) {
+                fs.mkdirSync(folder);
+            }
+        }
+
+        cb(null, `uploads/${folder}`);
     },
     filename: (req, file, cb) => {
-
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-})
-
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+// Mini validação da imagem
 const imageUpload = multer({
     storage: imageStorage,
+
+    // função callback que verifica a extensão do arquivo
     fileFilter(req, file, cb) {
+
         if (!file.originalname.match(/\.(png|jpg)$/)) {
 
             // upload only png and jpg formats
