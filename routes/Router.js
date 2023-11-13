66873b247e4f997a75a2f23bb51fe46e8ae0a810
Dayane Ramos <dayanecode@gmail.com>
2023-11-13 20:22:17 -0300

    add comments

--- routes/Router.js
+++ routes/Router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express();
 
 router.use("/api/users", require("./UserRoutes"))
+// Habilitando as rotas de fotos na chamada da API 
 router.use("/api/photos", require("./PhotoRoutes"))
 
 // test route
