29a52cb89d8806e878c72a4503fde047a74a21a5
Dayane Ramos <dayanecode@gmail.com>
2023-11-28 17:25:33 -0300

    created photo update validation

--- middlewares/photoValidation.js
+++ middlewares/photoValidation.js
@@ -20,6 +20,19 @@ const photoInsertValidation = () => {
 
 }
 
+const photoUpdateValidation = () => {
+    return [
+        body("title")
+            .optional()
+            .isString()
+            .withMessage("O título é obrigatório.")
+            .isLength({ min: 3 })
+            .withMessage("O título precisa conter no mínimo 3 caracteres."),
+    ]
+}
+
+
 module.exports = {
     photoInsertValidation,
+    photoUpdateValidation,
 }
