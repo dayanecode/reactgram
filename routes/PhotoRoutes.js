f7e96f000f34e4362e0926fc71ab2c9a1a989148
Dayane Ramos <dayanecode@gmail.com>
2023-11-28 17:26:55 -0300

    created router get photo by id and like photo

--- routes/PhotoRoutes.js
+++ routes/PhotoRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const router = express.Router()
 
-// Controller
-const { insertPhoto, deletePhoto } = require("../controllers/PhotoController")
+// Controller (rotas de fotos)
+const {
+    insertPhoto,
+    deletePhoto,
+    getAllPhotos,
+    getUserPhotos,
+    getPhotoById,
+    updatePhoto,
+    likePhoto } = require("../controllers/PhotoController")
 
 // Middlewares
-const { photoInsertValidation } = require("../middlewares/photoValidation")
+const { photoInsertValidation, photoUpdateValidation } = require("../middlewares/photoValidation")
 const authGuard = require("../middlewares/authGuard")
 const validate = require("../middlewares/handleValidation")
 const { imageUpload } = require("../middlewares/imageUpload");
@@ -21,5 +28,10 @@ router.post("/",
 );
 
 router.delete("/:id", authGuard, deletePhoto)
+router.get("/", authGuard, getAllPhotos)
+router.get("/user/:id", authGuard, getUserPhotos)
+router.get("/:id", authGuard, getPhotoById)
+router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto)
+router.put("/like/:id", authGuard, likePhoto)
 
 module.exports = router;
