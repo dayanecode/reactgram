929ff30a5717321597bff94f23c870399292b302
Dayane Ramos <dayanecode@gmail.com>
2024-02-15 15:01:12 -0300

    inserted like, comment and search functionalities in middlewares and photo routes

--- routes/PhotoRoutes.js
+++ routes/PhotoRoutes.js
@@ -9,10 +9,12 @@ const {
     getUserPhotos,
     getPhotoById,
     updatePhoto,
-    likePhoto } = require("../controllers/PhotoController")
+    likePhoto, 
+    commentPhoto,
+    searchPhotos} = require("../controllers/PhotoController")
 
 // Middlewares
-const { photoInsertValidation, photoUpdateValidation } = require("../middlewares/photoValidation")
+const { photoInsertValidation, photoUpdateValidation, commentValidation } = require("../middlewares/photoValidation")
 const authGuard = require("../middlewares/authGuard")
 const validate = require("../middlewares/handleValidation")
 const { imageUpload } = require("../middlewares/imageUpload");
@@ -27,11 +29,15 @@ router.post("/",
     insertPhoto
 );
 
+// Importante respeitar as ordens(sequencias) das Rotas para não haver problemas no retorno da API
 router.delete("/:id", authGuard, deletePhoto)
 router.get("/", authGuard, getAllPhotos)
 router.get("/user/:id", authGuard, getUserPhotos)
+router.get("/search", authGuard, searchPhotos)
+
 router.get("/:id", authGuard, getPhotoById)
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto)
 router.put("/like/:id", authGuard, likePhoto)
+router.put("/comment/:id", authGuard, commentValidation(), validate, commentPhoto)
 
 module.exports = router;
